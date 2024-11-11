da6ee133c35919d4be69bf33c89ce63b0f59e791
rayoooo20 <alvaro.rayo@alu.uclm.es>
2024-11-11 20:29:20 +0100

    solucionado problema con javax.persistance. Modificado a jakarta.persistance

--- library/src/main/java/es/uclm/library/business/entity/CartaMenu.java
+++ library/src/main/java/es/uclm/library/business/entity/CartaMenu.java
@@ -1,6 +1,6 @@
 package es.uclm.library.business.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Collection;
 
 @Entity
--- library/src/main/java/es/uclm/library/business/entity/ItemMenu.java
+++ library/src/main/java/es/uclm/library/business/entity/ItemMenu.java
@@ -1,6 +1,6 @@
 package es.uclm.library.business.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "item_menu")
