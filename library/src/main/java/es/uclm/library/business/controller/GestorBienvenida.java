1632503b53c417fbfa9cb1bf6ab9ce8dfe89b572
MrJotaa <realjcd@gmail.com>
2024-11-20 17:30:35 +0100

    4ª correccion del gestor bienvenida

--- library/src/main/java/es/uclm/library/business/controller/GestorBienvenida.java
+++ library/src/main/java/es/uclm/library/business/controller/GestorBienvenida.java
@@ -3,8 +3,6 @@ package es.uclm.library.business.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-package com.example.demo.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GestorBienvenida {
 
     private static final Logger logger = LoggerFactory.getLogger(GestorBienvenida.class);
-
-    @GetMapping("/Bienvenida")
-    public String welcome() {
-        logger.info("Handling welcome request");
-        return "Bienvenida";
-    }
-}
     
     @GetMapping("/Bienvenida")
     public String bienvenida(Model model) {
-        // Agregamos un mensaje para que lo muestre en la plantilla
-        model.addAttribute("mensaje", "¡Bienvenido a la aplicación!");
+        logger.info("Handling welcome request");
+	model.addAttribute("mensaje", "¡Bienvenido a la aplicación!");
         return "Bienvenida"; // El nombre de la plantilla sin la extensión .html
     }
 }
